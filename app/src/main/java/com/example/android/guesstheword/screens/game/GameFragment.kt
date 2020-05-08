634f1314713c6fcf11f9742ae71f594ea35e6dc8
vuth1 <vubao108@gmail.com>
2020-05-09 05:08:56 +0700

    use livedata with backing property to encapsulate

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -23,6 +23,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment
@@ -54,12 +55,17 @@ class GameFragment : Fragment() {
 
         Timber.i("Called ViewModelProviders.of")
         viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
-
+        viewModel.score.observe(viewLifecycleOwner, Observer {
+            newScore-> binding.scoreText.text = newScore.toString()
+        })
+        viewModel.word.observe(viewLifecycleOwner, Observer {
+            newWord-> binding.wordText.text = newWord
+        })
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
         binding.endGameButton.setOnClickListener { onEndGame() }
-        updateScoreText()
-        updateWordText()
+        //updateScoreText()
+        //updateWordText()
         return binding.root
 
     }
@@ -68,25 +74,27 @@ class GameFragment : Fragment() {
 
     private fun onSkip() {
         viewModel.onSkip()
-        updateWordText()
-        updateScoreText()
+        //updateWordText()
+       // updateScoreText()
     }
 
     private fun onCorrect() {
         viewModel.onCorrect()
-        updateWordText()
-        updateScoreText()
+        //updateWordText()
+        //updateScoreText()
     }
 
     /** Methods for updating the UI **/
-
+    /*
     private fun updateWordText() {
-        binding.wordText.text = viewModel.word
+        binding.wordText.text = viewModel.word.value
     }
 
     private fun updateScoreText() {
-        binding.scoreText.text = viewModel.score.toString()
+        binding.scoreText.text = viewModel.score.value.toString()
     }
+    */
+     
 
     private fun onEndGame(){
         gameFinished()
@@ -94,7 +102,7 @@ class GameFragment : Fragment() {
     private fun gameFinished(){
         Toast.makeText(activity, "game has just finished",Toast.LENGTH_SHORT).show()
         val action = GameFragmentDirections.actionGameToScore()
-        action.score = viewModel.score
+        action.score = viewModel.score.value?:0
         NavHostFragment.findNavController(this).navigate(action)
     }
 }
--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -2,15 +2,19 @@ package com.example.android.guesstheword.screens.game
 
 import android.util.Log
 import androidx.core.view.ViewCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
 class GameViewModel: ViewModel() {
-     var word = ""
-
+    private val _word = MutableLiveData<String>()
+    val word:LiveData<String>
+        get() = _word
     // The current score
-     var score = 0
-
+    private val _score = MutableLiveData<Int>()
+    val score:LiveData<Int>
+        get() = _score
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
     private fun resetList() {
@@ -41,6 +45,8 @@ class GameViewModel: ViewModel() {
     }
     init{
         //Log.i("GameViewModal","GameViewModel created")
+        _word.value = ""
+        _score.value = 0
         resetList()
         nextWord()
 
@@ -49,18 +55,18 @@ class GameViewModel: ViewModel() {
     private fun nextWord() {
         if (!wordList.isEmpty()) {
             //Select and remove a word from the list
-            word = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
 
     }
     /** Methods for buttons presses **/
     fun onSkip() {
-        score--
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 
